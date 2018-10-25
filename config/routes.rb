1698038d47cd1df9506bbbfae43ac31d230d983a
Jimmy Smith <jaystansmith@gmail.com>
2018-10-25 11:51:05 -0600

    Added code for cart functionality

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create, :show]
 
+  resources :carts, only: [:create]
+
   namespace :admin do
     resources :categories, only: [:index]
   end
